b50d7da67e0f553caf89aa0117d848205309f006
Md. Sayed Uddin Rayhan <67170635+SayedUddinRayhan@users.noreply.github.com>
2023-03-04 11:25:58 +0000

    Added some style on form.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let newTask = document.querySelector('#new-task');
 let form = document.querySelector('form');
-let todoUl = document.querySelector('#items');
- let completeUl = document.querySelector('.complete-list ul');
+let todoUl = document.querySelector('#items1');
+let completeUl = document.querySelector('.complete-list ul');
 
 
-    
